cdf0e082c0f5bc1f83bcb62fb01306769a677f7b
jlvilasoler <jlvilasoler@hotmail.com>
2023-06-29 14:24:15 -0300

    se arregla link to /Cart

--- src/components/CardWidget/CardWidget.jsx
+++ src/components/CardWidget/CardWidget.jsx
@@ -1,13 +1,15 @@
 import "../CardWidget/CardWidget.scss"
 import { useState } from "react"
 import { Link } from "react-router-dom";
+
+
 const CardWidget = () => {
 
     const [counter, setCounter] = useState(0);
 
     return (
         <div className="CardWidget">
-        <Link className="CardWidget_icono" href="/Cart"> 🛒{counter} </Link>
+        <Link className="CardWidget_icono" to="/Cart"> 🛒{counter} </Link>
         </div>
 )
 }
